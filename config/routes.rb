c30a94c7cac34fd3f7ca0570268eb3ff0f02476d
Efflam Castel <efflamm.castel@gmail.com>
2015-01-27 01:18:26 +0100

    1st release

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  resources :comments, only: [:new, :create]
+
+  root to: 'comments#new'
+
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get 'auth/failure', to: 'sessions#auth_fail'
+  get '/sign_out', to: 'sessions#destroy', as: :sign_out
+end
